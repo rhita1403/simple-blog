be581ff10ee81ac7f2f3d9e882ec5864a1603b92
Rhita Lamdouar <rhita.lamdouar@hotmail.com>
2024-01-22 16:40:03 +0100

    simple blog created

---
+++ app/javascript/application.js
@@ -0,0 +1,23 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "trix"
+import "@rails/actiontext"
+
+import '../stylesheets/application';
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+    let element = event.target.closest('.paragraph-form')
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+})
